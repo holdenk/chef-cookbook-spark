626bcb89f9d6e160a693191d409b1ab6bc12b9b8
Holden Karau <holden@pigscanfly.ca>
2013-04-13 14:49:23 -0700

    Update meta data

--- cookbook/cookbook-spark/metadata.rb
+++ cookbook/cookbook-spark/metadata.rb
@@ -1,7 +1,7 @@
 name             'cookbook-spark'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Holden Karau'
+maintainer_email 'holden@pigscanfly.ca'
+license          'BSD 3 clause'
 description      'Installs/Configures cookbook-spark'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
