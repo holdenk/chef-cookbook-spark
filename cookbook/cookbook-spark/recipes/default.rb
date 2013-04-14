5c421fcbdf132a22eab6d08bbdb8f3aedca1917c
Holden Karau <holden@pigscanfly.ca>
2013-04-13 21:02:27 -0700

    Fix s/:user/:username/ and make permissions more open

--- cookbook/cookbook-spark/recipes/default.rb
+++ cookbook/cookbook-spark/recipes/default.rb
@@ -7,12 +7,43 @@
 # BSD-3 Clause
 #
 
+# Create the home directory
+package("git")
+directory node[:spark][:home] do
+  owner node[:spark][:username]
+  group node[:spark][:username]
+  mode 744
+  action :create
+end
+
+# Override some java settings
+node.default['java']['oracle']['accept_oracle_download_terms'] = true
+node.default['java']['install_flavor'] = 'oracle'
+# Specify the scala version we are using
 node.default[:scala][:version] = "2.9.3"
 node.default[:scala][:url] = "http://www.scala-lang.org/downloads/distrib/files/scala-2.9.3.tgz"
+# Create the scala directory
+directory "{node[:spark][:home]}/scala" do
+  owner node[:spark][:username]
+  group node[:spark][:username]
+  mode 0744
+  action :create
+end
 node.default[:scala][:home] = node[:spark][:home]+"/scala"
+# Install scala
 include_recipe "scala::default"
 
-git "{node[:spark][:home]}/spark/" do
+# Clone 
+node.default[:spark][:spark_path] = node[:spark][:home]+"/spark"
+directory node[:spark][:spark_path] do
+  owner node[:spark][:username]
+  group node[:spark][:username]
+  mode 0744
+  action :create
+end
+git node[:spark][:spark_path] do
+  user node[:spark][:username]
+  group node[:spark][:username]
   repository node[:spark][:git_repository]
   reference node[:spark][:git_revision]
   action :sync
@@ -20,24 +51,24 @@ git "{node[:spark][:home]}/spark/" do
 end
 
 bash "build_spark" do
-  cwd "{node[:spark][:home]}"
-  user node[:spark][:user]
+  user node[:spark][:username]
   group node[:spark][:group]
+  cwd node[:spark][:spark_path]
   code <<-EOH
     ./sbt/sbt package
   EOH
 end
 
-template "{node[:spark][:home]}/spark/conf/spark-env.sh" do
+template node[:spark][:spark_path]+"/conf/spark-env.sh" do
   source "spark-env.sh.erb"
-  owner node[:spark][:user]
+  owner node[:spark][:username]
   group node[:spark][:group]
   mode 00600
 end
 
-template "{node[:spark][:home]}/spark/conf/slaves" do
+template node[:spark][:spark_path]+"/conf/slaves" do
   source "slaves.erb"
-  owner node[:spark][:user]
+  owner node[:spark][:username]
   group node[:spark][:group]
   mode 00600
 end
